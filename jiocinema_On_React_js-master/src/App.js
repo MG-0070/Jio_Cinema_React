befcb2e4db27b1c63c1acf1fb98bc84097614415
Gupta_Rishi <108511606+MG-0070@users.noreply.github.com>
2023-05-17 15:46:17 +0530

    Add files via upload

---
+++ jiocinema_On_React_js-master/src/App.js
@@ -0,0 +1,13 @@
+
+import Navbar from './Components/Navbar';
+import Slider from './Components/Slider';
+function App() {
+  return (
+    <div className="App">
+ <Navbar/>
+ <Slider/>
+    </div>
+  );
+}
+
+export default App;
